d97a1081db2f2f7b482a1774874cc97a71da4f54
BAM <bsharamin12@gmail.com>
2024-04-07 20:15:59 +0300

    useage example

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Fragment, useState } from "react";
+import "./App.css";
+import Suspenser from "./components/Suspenser";
+
+function App() {
+  const [data, setData] = useState({ name: null });
+  return (
+    <Suspenser
+      setData={setData}
+      fetch={
+        new Promise<{ data: string }>((res, rej) => {
+          // setTimeout(() => {
+          //   res([
+          //     {
+          //       name: "data",
+          //     },
+          //     {
+          //       name: "second",
+          //     },
+          //   ]);
+          // }, 3000);
+          rej("err");
+        })
+      }
+      renderElement={(data) => {
+        return (
+          <>
+            {data.map((item, index) => (
+              <Fragment key={index}>{item.name}</Fragment>
+            ))}
+          </>
+        );
+      }}
+    >
+      {/* {data?.name} */}
+    </Suspenser>
+  );
+}
+export default App;
